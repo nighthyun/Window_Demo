750fe8a15168ebc7fa3308f0662a20431571fab5
nighthyun <c0956625343@hotmail.com.tw>
2024-06-13 14:18:26 +0800

    first commit

---
+++ src/Window_Demo.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Window_Demo {
+	public static void main(String[] args){
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setSize(400,200);	//建立視窗大小
+		window.setVisible(true);	//視窗顯示
+	}
+}
